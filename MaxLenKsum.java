19938f1f205ea8a3f879031c489664fc2f23415a
Srividhya <167010612+SrividhyaSan@users.noreply.github.com>
2025-09-23 16:16:55 +0530

    Update MaxLenKsum.java

--- MaxLenKsum.java
+++ MaxLenKsum.java
@@ -2,7 +2,7 @@ package StudyOpedia;
 
 public class MaxLenKsum {
     public static void main(String[] args) {
-        int a[] = {2,5,1,7,10}; // Longest subarray when sum<=k
+        int a[] = {7,5,1,2,10}; // Longest subarray when sum<=k
         int k = 14, sum = 0,maxsum=0, maxlen = 0, i = 0, j = 0, n = a.length,index1=0,index2=0;
         while (j < n) {
             sum += a[j];
@@ -11,15 +11,15 @@ public class MaxLenKsum {
                 i++;
             }
             if (sum <= k) {
-                if((sum > maxsum && maxlen == j - i + 1)){
-                index1 = i;
-                index2 = j;
+                if ((sum > maxsum && maxlen == j - i + 1) || j - i + 1 > maxlen) {
+                    maxlen = j - i + 1;
+                    index1 = i;
+                    index2 = j;
                 }
-                maxlen = Math.max(maxlen, j - i + 1);
                 maxsum = Math.max(maxsum, sum);
             }
             j++;
-
+        }
             System.out.println("maxlen=" + maxlen + " index1=" + index1 + " index2=" + index2 + " maxsum=" + maxsum);
-        } }
+        }
 }
