7ed46a775023ec8042f43ee6b855f64ee207f8c3
Srividhya <167010612+SrividhyaSan@users.noreply.github.com>
2025-10-02 19:20:02 +0530

    Update LongestSubstringWithkDistinct.java

--- LongestSubstringWithkDistinct.java
+++ LongestSubstringWithkDistinct.java
@@ -20,42 +20,60 @@ The length of the string 4.
 
 Input : s = "abccab" , k = 4
 
-package StudyOpedia;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LongestSubstringWithkDistinct {
     public static void main(String args[]){
-        String s="abccab";
-        int i=0,k=4,j,maxlen=0,n=s.length();
-        ArrayList<Character> chr=new ArrayList<>();
-        for(j=0;j<n;j++){
-            char ch=s.charAt(j);
-            if(!chr.contains(ch) && chr.size()<k){
-                chr.add(ch);
-            }
-            else if(chr.size()==k){
-                break;
-            }
-        }
-        maxlen = Math.max(maxlen, j - i);
-        System.out.print(Arrays.toString(chr.toArray())+j);
-        while(j<n){
-            char ch=s.charAt(j);
-            if(chr.size()<=k){
-                maxlen = Math.max(maxlen, j - i + 1);
-            }
-            if(!chr.contains(ch) && chr.size()==k){
-                chr.remove((Character) s.charAt(i));
-                i++;
-                chr.add(s.charAt(i));
-            }
+        String s="dcdabce"; // aababbcaacc
+        int i=0,k=3,j=0,maxlen=0,n=s.length();
+        // Using  map
+//       Map<Character,Integer> map=new HashMap<>();
+//       while(j<n){
+//           char chr=s.charAt(j);
+//           map.put(chr,map.getOrDefault(chr,0)+1);
+//           if(map.size()>k){
+//               map.put(s.charAt(i),map.get(s.charAt(i))-1);
+//               if(map.get(s.charAt(i))==0){
+//                   map.remove( s.charAt(i));
+//               }
+//               i++;
+//           }
+//           if(map.size()<=k)
+//               maxlen=Math.max(maxlen,j-i+1);
+//           j++;
+//       }
+        ArrayList<Character> distinctChars = new ArrayList<>();
 
+        while (j < n) {
+            char ch = s.charAt(j);
+
+            // Add new character if it's not already in the list
+            if (!distinctChars.contains(ch)) {
+                distinctChars.add(ch);
+            }
+            // If we exceed k distinct characters, shrink the window
+            while (distinctChars.size() > k) {
+                char startChar = s.charAt(i);
+                // Remove startChar only if it's not repeated in the window
+                boolean stillExists = false;
+                for (int m = i + 1; m <= j; m++) {
+                    if (s.charAt(m) == startChar) {
+                        stillExists = true;
+                        break;
+                    }
+                }
+                if (!stillExists) {
+                    distinctChars.remove((Character) startChar);
+                }
+                i++;
+            }
+            maxlen = Math.max(maxlen, j - i + 1);
             j++;
         }
-        System.out.println(maxlen);
+
+        System.out.println("maxlen  "+maxlen );
     }
 }
-
