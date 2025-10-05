afad69208132b268355f646e2a2b4e796eff6078
Srividhya <167010612+SrividhyaSan@users.noreply.github.com>
2025-10-05 13:09:38 +0530

    Add files via upload

---
+++ LongestNegPosSubaryKsum.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestNegPosSubaryKsum {
+    public static void main(String args[]){
+       int a[]={1,2,3,-2,5,-1,0,-1};
+       int i,maxlen=0,sum=0,k=3;
+       Map<Integer,Integer> prefixsumMap=new HashMap<>();
+       for(i=0;i<a.length;i++){
+           sum+=a[i];
+           if(sum==k){
+               maxlen=i+1;
+           }
+           int prefixsum=sum-k;
+           if (prefixsumMap.containsKey(prefixsum)){
+               maxlen = Math.max(maxlen, i - prefixsumMap.get(prefixsum));
+           }
+           prefixsumMap.put(sum,i);
+       }
+       System.out.println("maxlen "+maxlen);
+    }
+}
